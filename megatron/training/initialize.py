8add33fa43d214a170a0d2b6c4c41f0fd536fe1f
Anastasiia Demidova <49967368+profii@users.noreply.github.com>
2024-04-13 22:30:40 +0400

    Update initialize.py

--- megatron/training/initialize.py
+++ megatron/training/initialize.py
@@ -43,7 +43,10 @@ def initialize_megatron(
 
     # Parse arguments
     args = parse_args(extra_args_provider, ignore_unknown_args)
-
+    if args.rank == 0:
+        print("> set_device 0 ...", flush=True)
+        torch.cuda.set_device(args.local_rank)
+        
     if args.use_checkpoint_args or args_defaults.get("use_checkpoint_args", False):
         assert args.load is not None, "--use-checkpoints-args requires --load argument"
         load_args_from_checkpoint(args)
