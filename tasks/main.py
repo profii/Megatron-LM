31e02a1cf4a3b69d349b9cab7927e220d2096a82
Anastasiia Demidova <49967368+profii@users.noreply.github.com>
2024-04-13 18:42:39 +0400

    Update main.py local_

--- tasks/main.py
+++ tasks/main.py
@@ -71,7 +71,7 @@ def get_tasks_args(parser):
                         help='Av.rank validation: how many other negatives to'
                         ' take from each question pool')
 
-    group.add_argument('--local-rank', type=int, default=int(os.environ["LOCAL_RANK"]),
+    group.add_argument('--local_rank', type=int, default=int(os.environ["LOCAL_RANK"]),
                         help='Local-rank')
 
     return parser
